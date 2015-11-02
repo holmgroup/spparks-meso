021a154dc27b1e06a501b45382e34ecb98d643b6
Brian DeCost <bdecost@andrew.cmu.edu>
2015-11-02 14:25:05 -0500

    changed to 4-space python indent.

--- mesotools/io.py
+++ mesotools/io.py
@@ -9,49 +9,49 @@ QUAT_PATH = DATA_PATH + '/CellFeatureData/AvgQuats'
 DIM_PATH = DATA_PATH+'/DIMENSIONS'
 
 def load_dream3d(path, renumber_grains=False):
-  with h5py.File(path, 'r') as f:
-    grain_ids = np.array(f[GRAIN_ID_PATH], dtype=np.int)
-  shape = tuple([s for s in grain_ids.shape if s > 1])
-  if renumber_grains:
-    grain_ids = renumber(grain_ids)
-  return grain_ids.reshape(shape)
+    with h5py.File(path, 'r') as f:
+        grain_ids = np.array(f[GRAIN_ID_PATH], dtype=np.int)
+    shape = tuple([s for s in grain_ids.shape if s > 1])
+    if renumber_grains:
+        grain_ids = renumber(grain_ids)
+    return grain_ids.reshape(shape)
 
 
 def renumber(grains, start_from=1):
-  grain_ids = np.unique(grains)
-  new_grains = np.zeros_like(grains)
-  for idx, grain_id in enumerate(grain_ids):
-    new_grains[grains == grain_id] = idx+start_from
-  return new_grains
+    grain_ids = np.unique(grains)
+    new_grains = np.zeros_like(grains)
+    for idx, grain_id in enumerate(grain_ids):
+        new_grains[grains == grain_id] = idx+start_from
+    return new_grains
 
 
 def save_dream3d(path, grains, quaternions):
-  with h5py.File(path, 'w') as f:
-    f.attrs['FileVersion'] = np.string_('6.0\x00')
-    f[GRAIN_ID_PATH] = grains
-    f[QUAT_PATH] = quaternions
-    f[DIM_PATH] = np.array([s for s in grains.shape])
-  return
+    with h5py.File(path, 'w') as f:
+        f.attrs['FileVersion'] = np.string_('6.0\x00')
+        f[GRAIN_ID_PATH] = grains
+        f[QUAT_PATH] = quaternions
+        f[DIM_PATH] = np.array([s for s in grains.shape])
+    return
 
 
 def load_quaternions(path):
-  with h5py.File(path, 'r') as f:
-    quaternions = np.array(f[QUAT_PATH], dtype=np.float32)
-  return quaternions
+    with h5py.File(path, 'r') as f:
+        quaternions = np.array(f[QUAT_PATH], dtype=np.float32)
+    return quaternions
 
 def dream3d_to_sites(dream3d_path, sites_path):
-  """ convert DREAM3D microstructure to SPPARKS sites format """
-  grains = load_dream3d(dream3d_path)
-  with open(sites_path, 'w') as f:
-    print('# SPPARKS sites file', file=f)
-    print('{} dimension'.format(grains.ndim), file=f)
-    print('{} sites'.format(grains.size), file=f)
-    print('0 {} xlo xhi'.format(grains.shape[0]), file=f)
-    print('0 {} ylo yhi'.format(grains.shape[1]), file=f)
-    print('-.5 .5 zlo zhi', file=f)
-    print('', file=f)
-    print('Values', file=f)
-    print('', file=f) # empty line is not optional
-    for (index,), site in np.ndenumerate(grains.flatten()):
-      print('{} {}'.format(index+1, site), file=f)
-  return
+    """ convert DREAM3D microstructure to SPPARKS sites format """
+    grains = load_dream3d(dream3d_path)
+    with open(sites_path, 'w') as f:
+        print('# SPPARKS sites file', file=f)
+        print('{} dimension'.format(grains.ndim), file=f)
+        print('{} sites'.format(grains.size), file=f)
+        print('0 {} xlo xhi'.format(grains.shape[0]), file=f)
+        print('0 {} ylo yhi'.format(grains.shape[1]), file=f)
+        print('-.5 .5 zlo zhi', file=f)
+        print('', file=f)
+        print('Values', file=f)
+        print('', file=f) # empty line is not optional
+        for (index,), site in np.ndenumerate(grains.flatten()):
+            print('{} {}'.format(index+1, site), file=f)
+        return
--- mesotools/nxjson.py
+++ mesotools/nxjson.py
@@ -16,98 +16,107 @@ import networkx as nx
 # mlab = matlab.engine.start_matlab()
 # print('matlab loaded.')
 
+
 def adjacency_matrix(graph):
-  """ am is a list of adjacency matrices.
-      nx -> numpy -> list of lists """
-  am = nx.to_numpy_matrix(graph)
-  am = np.asarray(am)
-  # json needs python int, not np.int
-  return list( list( map(float, row)) for row in am )
+    """ am is a list of adjacency matrices.
+        nx -> numpy -> list of lists """
+    am = nx.to_numpy_matrix(graph)
+    am = np.asarray(am)
+    # json needs python int, not np.int
+    return list(list(map(float, row)) for row in am)
+
 
 def adjacency_list(graph):
-  """ al is a list of adjacency lists
-      element i: list of adjacent nodes for node i"""
-  return [ [float(edge) for edge in graph[node]]for node in graph ] 
+    """ al is a list of adjacency lists
+        element i: list of adjacent nodes for node i"""
+    return [[float(edge) for edge in graph[node]]for node in graph]
+
 
 def node_labels(graph, label_key=None):
-  """ nl is a list of dictionaries containing node labels
-      each dict key corresponds to a node attribute
-      Borgwardt code expects values to be a column vector of doubles
-      write as a single list here and transpose in MATLAB
-      these keys map to a list of doubles, one per node
-  """
-  def _label(node_data):
-    try:
-      return node_data[label_key]
-    except KeyError:
-      return 1.0
+    """ nl is a list of dictionaries containing node labels
+        each dict key corresponds to a node attribute
+        Borgwardt code expects values to be a column vector of doubles
+        write as a single list here and transpose in MATLAB
+        these keys map to a list of doubles, one per node
+    """
+    def _label(node_data):
+        try:
+            return node_data[label_key]
+        except KeyError:
+            return 1.0
 
-  return {'values': list( _label(data) for __,data in graph.nodes(data=True) )}
+    return {'values': list(_label(data) for __, data in graph.nodes(data=True))}
+
 
 def graph_labels(graphs):
-  ''' return column vector of labels '''
-  labels = list( float(1) for g in graphs )
-  return labels
+    ''' return column vector of labels '''
+    labels = list(float(1) for g in graphs)
+    return labels
+
 
 def convert_graph(graph):
-  """ Borgwardt's code uses consecutive integers starting at 1
-        for node labels (represented as float64).
-        Preserve ordering of nodes in graph """
-  return nx.convert_node_labels_to_integers(graph, first_label=1)
-  
+    """ Borgwardt's code uses consecutive integers starting at 1
+          for node labels (represented as float64).
+          Preserve ordering of nodes in graph """
+    return nx.convert_node_labels_to_integers(graph, first_label=1)
+
 
 def nx_to_borgwardt(graphs, label_key=None, name='data',):
-  ''' convert a list of networkx graphs to Borgwardt's MATLAB format.
-      am: adjacency matrix
-      al: adjacency list
-      nl.values: column vector of node labels
-      el: contains edge labels (optional, excluding for now)
-      parameter name='data': don't change this without changing nxjson.m
-  '''
-  converted = [convert_graph(graph) for graph in graphs]
-  
-  dataset = [ { 'nl': node_labels(g, label_key=label_key),
-                'al': adjacency_list(g), 
-                'am': adjacency_matrix(g) } for g in converted ]
+    ''' convert a list of networkx graphs to Borgwardt's MATLAB format.
+        am: adjacency matrix
+        al: adjacency list
+        nl.values: column vector of node labels
+        el: contains edge labels (optional, excluding for now)
+        parameter name='data': don't change this without changing nxjson.m
+    '''
+    converted = [convert_graph(graph) for graph in graphs]
 
-  labels = graph_labels(converted)
+    dataset = [{'nl': node_labels(g, label_key=label_key),
+                'al': adjacency_list(g),
+                'am': adjacency_matrix(g)} for g in converted]
 
-  return {'data': dataset, 'labels': labels}
+    labels = graph_labels(converted)
+
+    return {'data': dataset, 'labels': labels}
+
 
 def graphlist_to_json(graphs, path='test.json', node_label_key=None):
-  """ serialize a list of networkx graphs into a json format
-        that can be parsed by MATLAB using nxjson.m into the
-        format used by Bordwardt's graph kernel code
-  """
-  data = nx_to_borgwardt(graphs, label_key=node_label_key)
+    """ serialize a list of networkx graphs into a json format
+          that can be parsed by MATLAB using nxjson.m into the
+          format used by Bordwardt's graph kernel code
+    """
+    data = nx_to_borgwardt(graphs, label_key=node_label_key)
 
-  with open(path, 'w') as jf:
-    # json.dump(data, jf, indent=2)
-    print(json.dumps(data), file=jf)
-  return
+    with open(path, 'w') as jf:
+        # json.dump(data, jf, indent=2)
+        print(json.dumps(data), file=jf)
+    return
 
 
 ####################
 # helper functions #
 ####################
 def random_graphs(n_graphs=10):
-  g = [nx.gnp_random_graph(5, 0.8) for i in range(n_graphs)]
-  return g
+    g = [nx.gnp_random_graph(5, 0.8) for i in range(n_graphs)]
+    return g
+
 
 def star_graphs(n_graphs=10):
-  g = [nx.star_graph(i) for i in range(3,n_graphs+3)]
-  return g
+    g = [nx.star_graph(i) for i in range(3, n_graphs + 3)]
+    return g
+
 
 def atlas():
-  from networkx.generators.atlas import graph_atlas_g
-  graphs = graph_atlas_g()
-  graphs = [g for g in graphs if (g.size() > 5 and nx.is_connected(g))]
-  return graphs
+    from networkx.generators.atlas import graph_atlas_g
+    graphs = graph_atlas_g()
+    graphs = [g for g in graphs if (g.size() > 5 and nx.is_connected(g))]
+    return graphs
+
 
 def test_dataset():
-  print('writing test graph dataset to ./test.json')
-  graphs = star_graphs(n_graphs=3)
-  graphlist_to_json(graphs, path='test.json')
+    print('writing test graph dataset to ./test.json')
+    graphs = star_graphs(n_graphs=3)
+    graphlist_to_json(graphs, path='test.json')
 
 if __name__ == '__main__':
-  test_dataset()
+    test_dataset()
